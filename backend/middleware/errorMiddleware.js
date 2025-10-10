233063c59dd2e56e05cc90a8a28dd3b740e9be8b
Praneeth Chandra <spraneethchandra123@gmail.com>
2025-10-11 03:27:40 +0530

    improving security

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,10 +1,10 @@
 const notFound = (req, res, next) => {
-    const error = new error(`Not Found - ${req.originalUrl}`)
+    const error = new Error(`Not Found - ${req.originalUrl}`)
     res.status(404);
     next(error);
 }
 
-const errorHandler = (err, req, res, nest) => {
+const errorHandler = (err, req, res, next) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
@@ -15,7 +15,7 @@ const errorHandler = (err, req, res, nest) => {
 
     res.status(statusCode).json({
         message,
-        stack: process.env.NODE_ENV === 'production' ? numm : err.stack
+        stack: process.env.NODE_ENV === 'production' ? null : err.stack
     });
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -163,7 +163,7 @@ function SignUp() {
                             <div className="2xl:flex my-12 items-center justify-center space-y-4 2xl:space-x-6 2xl:space-y-0">
                                 <button className="sm:ml-12 md:ml-3 lg:ml-5 xl:ml-16 2xl:ml-0 h-12 pl-6 pr-10 2xl:px-6 border-2 border-gray-300 rounded-full transition duration-300 hover:border-sky-400 active:bg-sky-300">
                                     <div className="relative flex items-center space-x-8 justify-center">
-                                        <img src="https://tailus.io/sources/blocks/social/preview/images/google.svg" className="absolute left-0 w-5" alt="google logo" />
+                                        <img src="https://www.svgrepo.com/show/303108/google-icon-logo.svg" className="absolute left-0 w-5" alt="google logo" />
                                         <span className="block w-max font-semibold tracking-wide text-white text-sm transition duration-300 group-hover:text-sky-600 sm:text-base">Continue with Google</span>
                                     </div>
                                 </button>
